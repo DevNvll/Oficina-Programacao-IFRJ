8f14870245bb33ae6fa98169073836e9e42ce149
Henrick Martins <hmartins505@gmail.com>
2016-03-29 15:41:07 -0300

    Segunda aula

--- Python/salario.py
+++ Python/salario.py
@@ -1,22 +1,22 @@
 nome = str(input("Digite o nome do funcionário: "))
-salarioB = float(input("Digite o salario base: "))
+salarioB = float(input("Digite o salário base: "))
 aumento = 0
 salarioBNovo = 0
 
-if (salarioB <= 280):
+if salarioB <= 280:
     aumento = salarioB*0.20
     salarioBNovo = salarioB + aumento
-if (salarioB > 280 and salarioB <= 700):
+elif (salarioB <= 700):
     aumento = salarioB*0.15
     salarioBNovo = salarioB + aumento
-if (salarioB > 700 and salarioB <= 1500):
+elif (salarioB <= 1500):
     aumento = salarioB*0.1
     salarioBNovo = salarioB + aumento
-if (salarioB > 1500):
+else:
     aumento = salarioB*0.05
     salarioBNovo = salarioB + aumento
 
-print("Nome do funcionario: " + str(nome))
-print("Salario antes do aumento: R$" + str(salarioB))
+print("Nome do funcionário: " + str(nome))
+print("Salário antes do aumento: R$" + str(salarioB))
 print("Valor do aumento: R$" + str(aumento))
-print("Salario bruto novo: R$" + str(salarioBNovo))
+print("Salário bruto novo: R$" + str(salarioBNovo))
